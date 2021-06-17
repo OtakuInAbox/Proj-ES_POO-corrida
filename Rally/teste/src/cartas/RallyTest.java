f39ae03c297b62e54de2bdd7abeb6df9309303fc
GonçaloRodrigues <79711917+OtakuInAbox@users.noreply.github.com>
2021-06-17 14:47:25 +0100

    Add files via upload

---
+++ Rally/teste/src/cartas/RallyTest.java
@@ -0,0 +1,14 @@
+package cartas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RallyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
